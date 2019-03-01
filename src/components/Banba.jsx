081c31ffa3d8c91a195f955e55f7f8d3e05ad9ff
ribo <roibeard.mac.unfraidh@gmail.com>
2019-03-01 11:28:14 +0100

    Create Component: Banba.jsx

---
+++ src/components/Banba.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+export default class Banba extends Component {
+  render() {
+    return (
+      <div id="kungfu">
+        <h1>B A N B A</h1>
+      </div>
+    );
+  }
+}
