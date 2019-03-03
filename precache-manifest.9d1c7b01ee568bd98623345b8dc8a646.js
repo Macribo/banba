36e24e13a3ff00b159d93c538ec161eb7109ae7a
ribo <roibeard.mac.unfraidh@gmail.com>
2019-03-03 04:12:38 +0100

    Updates

---
+++ precache-manifest.9d1c7b01ee568bd98623345b8dc8a646.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac04d4d44677b8a1c09ef25e2c296ed5",
+    "url": "/banba/static/media/mainBG.ac04d4d4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/banba/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8e50482f8800f50ce4bb",
+    "url": "/banba/static/js/runtime~main.8e50482f.js"
+  },
+  {
+    "revision": "a40fc98ec03f1d90cfd3",
+    "url": "/banba/static/js/main.a40fc98e.chunk.js"
+  },
+  {
+    "revision": "f35a5ec09ffef2998883",
+    "url": "/banba/static/js/2.f35a5ec0.chunk.js"
+  },
+  {
+    "revision": "a40fc98ec03f1d90cfd3",
+    "url": "/banba/static/css/main.82e208c0.chunk.css"
+  },
+  {
+    "revision": "f35a5ec09ffef2998883",
+    "url": "/banba/static/css/2.6b1ddee5.chunk.css"
+  },
+  {
+    "revision": "1d8e6bf83dd47f6882310d56c6eaaebb",
+    "url": "/banba/index.html"
+  }
+];
